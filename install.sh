6283c041d07320a0fa23c7a7bce6402ed225afd8
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 14:36:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-source ~/.bash_profile
+#sudo wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+#sudo tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
+#export PATH=$PATH:/usr/local/go/bin
+#source ~/.bash_profile
+echo "installed"
