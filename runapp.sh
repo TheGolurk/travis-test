0d050765b3d955919876fb1c08d341f6519074a0
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-15 22:06:49 -0500

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -6,6 +6,8 @@ cd /home/ec2-user/go/src/github.com/TheGolurk/travis-test
 export PATH=$PATH:/usr/local/go/bin
 source ~/.bash_profile
 
+go env GOCACHE="on"
+
 go env GOCACHE="/home/ec2-user/.cache/go-build"
 
 go env
