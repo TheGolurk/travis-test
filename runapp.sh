05e2de2cf38d1457858692a3f450b8562e748a46
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 18:48:40 -0500

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 pwd
 
-cmd=`$(ps -C "go run main.go" >/dev/null)`
-
-if $cmd > 0; then
-        echo "Don't exist"
-else
-        echo "Exist"
-        sudo kill %1
-fi
-
 export HOME=/home/ec2-user/
 
 cd /home/ec2-user/go/src/github.com/TheGolurk/travis-test
