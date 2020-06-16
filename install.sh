028cc40fe85407d65cfa6c5ae4bcc9efb57da800
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 18:26:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,14 @@
 #sudo tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
 #export PATH=$PATH:/usr/local/go/bin
 #source ~/.bash_profile
+
+cmd=`$(ps -C "go run main.go" >/dev/null)`
+
+if $cmd > 0; then
+        echo "Don't exist"
+else
+        echo "Exist"
+        kill %1
+fi
+
 echo "installed"
