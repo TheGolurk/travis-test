10ef68966de3f3b0f4a4b3c6bba6ef48969dc358
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 18:44:00 -0500

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 pwd
 
-jobs
-jobs
+cmd=`$(ps -C "go run main.go" >/dev/null)`
 
-sudo kill %1
+if $cmd > 0; then
+        echo "Don't exist"
+else
+        echo "Exist"
+        sudo kill %1
+fi
 
 export HOME=/home/ec2-user/
 
