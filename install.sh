c47583d00f3f89a1a8c0dcd0ddca4ba6f5371940
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 18:49:51 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@
 #export PATH=$PATH:/usr/local/go/bin
 #source ~/.bash_profile
 
-cmd=`$(ps -C "go run main.go" >/dev/null)`
+#cmd=`$(ps -C "go run main.go" >/dev/null)`
 
-if $cmd > 0; then
-        echo "Don't exist"
-else
-        echo "Exist"
-        sudo kill %1
-fi
+#if $cmd > 0; then
+#        echo "Don't exist"
+#else
+#        echo "Exist"
+#        sudo kill %1
+#fi
 
 echo "installed"
