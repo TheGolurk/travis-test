54b6aea537c8af74582ce9a805cf6084d0ed343d
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 10:47:58 -0500

    Update runapp.sh

--- runapp.sh
+++ runapp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 pwd
 
+export HOME=/home/ec2-user/
+
 cd /home/ec2-user/go/src/github.com/TheGolurk/travis-test
 
 echo $HOME
-echo pwd
+pwd
 
 export PATH=$PATH:/usr/local/go/bin
 source ~/.bash_profile
