d79bce9b6019f341d51b40d1f3368c8b099db326
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 14:19:09 -0500

    Update main.go

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func sayHello(w http.ResponseWriter, r *http.Request) {
 	case "javier":
 		name = "Javier, ola k ase"
 	default:
-		io.WriteString(w, "QUEDATE EN TU PUTA CASA lol")
+		io.WriteString(w, "QUEDATE EN TU PUTA CASA lol lol")
 		return
 	}
 	io.WriteString(w, fmt.Sprintf("Hello %s", name))
