42b568893426cfcfb1667b8f87accd07e05060c5
Christian Hernandez Najera <chrismatla68@gmail.com>
2020-06-16 19:06:08 -0500

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-cd /home/ec2-user/go/src/github.com/TheGolurk/travis-test
-
-cmd=`$(pgrep go)`
-
-if $cmd > 0; then
-        echo "Exist"
-        sudo kill cmd
-else
-        echo "Don't Exist"
-fi
+pkill go
+echo "deleted"
